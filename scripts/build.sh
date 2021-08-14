031c5e2fc921cab946366252c59c08f2efb59e18
Admir Trakic <atrakic@users.noreply.github.com>
2021-08-14 16:49:33 +0200

    Do not try to push images after building

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,7 +12,7 @@ source "$CURR_DIR/common.sh"
 SCRIPTNAME=$(basename "$0")
 echo "$SCRIPTNAME"
 
-#skaffold config set --global local-cluster true
+skaffold config set --global local-cluster true
 #skaffold config set --global default-repo k3d-registry.localhost:5000/skaffold
 
 skaffold version
